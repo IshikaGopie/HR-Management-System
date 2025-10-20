8de26f5e8511c54f91feb27898d799f730783478
IshikaGopie <ishikagopie123@gmail.com>
2025-10-20 14:14:22 -0400

    * added theme and updated table layout

--- HRPlusAssignment/obj/Debug/net8.0/HRPlusAssignment.AssemblyInfo.cs
+++ HRPlusAssignment/obj/Debug/net8.0/HRPlusAssignment.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRPlusAssignment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72037793b48ec5db57f6f58abad756297824e021")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24874aaa8eb71486f3cc674a7165e94c672bbea8")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRPlusAssignment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRPlusAssignment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
